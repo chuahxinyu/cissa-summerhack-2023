6fb73d233f3eea8315bace4af0b69283c637df55
vockoi <victoria.halim3@gmail.com>
2023-02-11 14:21:57 +1100

    Reorganize AboutMe section and add Profile field

--- src/components/InputDataSections/AboutMe.tsx
+++ src/components/InputDataSections/AboutMe.tsx
@@ -1,9 +1,7 @@
-import { Dispatch, SetStateAction } from 'react';
-import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import { Button, Container, Grid, Typography } from '@material-ui/core/';
+import {Grid, Typography } from '@material-ui/core/';
+import { IAboutMeSection} from '../types';
 import TextInputField from '../Forms/TextInputField';
-import { IAboutMeSection, IResumeData } from '../types';
 
 const INITIAL_FORM_STATE: IAboutMeSection = {
   name: '',
@@ -12,6 +10,7 @@ const INITIAL_FORM_STATE: IAboutMeSection = {
   phoneNo: '',
   address: '',
   jobTitle: '',
+  profile: '',
 };
 
 const ERROR_MESSAGE_SCHEMA = Yup.object().shape({
@@ -21,44 +20,58 @@ const ERROR_MESSAGE_SCHEMA = Yup.object().shape({
     phoneNo: Yup.number().typeError('Must be a number'),
     address: Yup.string(),
     jobTitle: Yup.string(),
+    profile: Yup.string(),
   });
 
 const AboutMe = () => {
   return (
-    <Grid container spacing={4}>
-      <Grid container item spacing={3} id="aboutMe">
-        <Grid container item spacing={2} id="name">
-          <Grid item xs={12}>
-            <Typography>Name</Typography>
-          </Grid>
-          <Grid item xs>
-            <TextInputField label="First Name" name="name" placeholder="e.g. Mai" />
-          </Grid>
-          <Grid item xs>
-            <TextInputField label="Last Name" name="lastName" placeholder="e.g. Pham" />
-          </Grid>
+    <Grid container item spacing={3} id="aboutMe">
+      <Grid item xs={12}>
+        <Typography variant="h3">About Me</Typography>
+      </Grid>
+
+      <Grid container item spacing={2} id="name">
+        <Grid item xs={12}>
+          <Typography>Name</Typography>
         </Grid>
-        <Grid container item spacing={2} id="contactDetails">
-          <Grid item xs={12}>
-            <Typography>Contact details</Typography>
-          </Grid>
-          <Grid item xs>
-            <TextInputField label="Email" name="email" placeholder="e.g. maiphs@potato.com" />
-          </Grid>
-          <Grid item xs>
-            <TextInputField label="Phone" name="phoneNo" placeholder="e.g. 0411111111" />
-          </Grid>
-          <Grid item xs>
-            <TextInputField label="Address" name="address" placeholder="e.g. 111 Potato Rd, Potato, POT 3000" />
-          </Grid>
+        <Grid item xs>
+          <TextInputField label="First Name" name="name" placeholder="e.g. Mai" />
         </Grid>
-        <Grid container item spacing={2} id="jobTitleField">
-          <Grid item xs={12}>
-            <Typography>Job Title</Typography>
-          </Grid>
-          <Grid item xs>
-            <TextInputField label="Job Title" name="jobTitle" placeholder="e.g. Graphic Designer" />
-          </Grid>
+        <Grid item xs>
+          <TextInputField label="Last Name" name="lastName" placeholder="e.g. Pham" />
+        </Grid>
+      </Grid>
+
+      <Grid container item spacing={2} id="contactDetails">
+        <Grid item xs={12}>
+          <Typography>Contact details</Typography>
+        </Grid>
+        <Grid item xs>
+          <TextInputField label="Email" name="email" placeholder="e.g. maiphs@potato.com" />
+        </Grid>
+        <Grid item xs>
+          <TextInputField label="Phone" name="phoneNo" placeholder="e.g. 0411111111" />
+        </Grid>
+        <Grid item xs>
+          <TextInputField label="Address" name="address" placeholder="e.g. 111 Potato Rd, Potato, POT 3000" />
+        </Grid>
+      </Grid>
+
+      <Grid container item spacing={2} id="jobTitleField">
+        <Grid item xs={12}>
+          <Typography>Job Title</Typography>
+        </Grid>
+        <Grid item xs>
+          <TextInputField label="Job Title" name="jobTitle" placeholder="e.g. Graphic Designer" />
+        </Grid>
+      </Grid>
+
+      <Grid container item spacing={2} id="profileField">
+        <Grid item xs={12}>
+          <Typography>Profile</Typography>
+        </Grid>
+        <Grid item xs>
+          <TextInputField label="Profile" name="profile" placeholder="e.g. Second year Media and Communications looking for opportunities in..." />
         </Grid>
       </Grid>
     </Grid>
