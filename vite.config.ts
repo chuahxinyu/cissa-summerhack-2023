995ea1298380fe89bc0f16dccf944ca842851347
Daniel Chin <wengjae@gmail.com>
2023-02-12 12:03:22 +1100

    wip github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://chuahxinyu.github.io/cissa-summerhack-2023",
   plugins: [react()],
 });
